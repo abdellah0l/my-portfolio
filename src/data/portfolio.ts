115367b5e60f16f719fa9cb1749c3cf68f036e5f
Maamra Abdellah <158842509+abdellah0l@users.noreply.github.com>
2025-09-07 21:03:29 +0100

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -58,7 +58,7 @@ export const projects: Project[] = [
     image: PortfolioProjectImage,
     stack: ['Next.js', 'Framer Motion', 'Tailwind CSS', 'Vite', 'TypeScript'],
     githubUrl: 'https://github.com/abdellah0l',
-    demoUrl: 'https://abdellahsh.space',
+    demoUrl: 'https://abdellahsp.space',
     featured: true,
   },
 ];
